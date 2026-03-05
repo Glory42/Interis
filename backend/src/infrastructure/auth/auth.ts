e1878e5042ece21d44ceda45c1d93baf9f09caa4
Glory42 <gorkemkaryol03@gmail.com>
2026-03-05 22:17:53 +0300

    feat: remake the betterauth connection.

---
+++ backend/src/infrastructure/auth/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../database/db";
+import * as schema from "../database/entities";
+
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "pg", 
+        schema: schema,
+    }),
+    emailAndPassword: {
+        enabled: true, 
+    },
+    secret: process.env.BETTER_AUTH_SECRET,
+    trustedOrigins: ["http://localhost:5173", "https://cinema.gorkemkaryol.dev"],
+});
